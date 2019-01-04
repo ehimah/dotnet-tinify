e2f14f05bd824f19566a680b93e88871c1e515a4
Ehimah Obuse <ehimahobuse@gmail.com>
2019-01-04 14:02:24 +0100

    added support for a destination path added support for aggregating images one path down the folder structure added resizing to the image compress specification

--- src/dotnet-tinify/ImageResizer.cs
+++ src/dotnet-tinify/ImageResizer.cs
@@ -1,7 +1,9 @@
 ﻿using McMaster.Extensions.CommandLineUtils;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using TinifyAPI;
 
@@ -23,6 +25,10 @@ namespace ImageOptimiser
         [Option(CommandOptionType.SingleValue, Description = "Your TinyPNG API key")]
         public string ApiKey { get; }
 
+        [Required(ErrorMessage = "You must specify the path to the target directory where the squashed images will be stored")]
+        [Argument(1, Name = "target", Description = "The path to the target directory where the squashed images will be stored")]
+        public string TargetPath { get; }
+
         public async Task<int> OnExecute(CommandLineApplication app, IConsole console)
         {
             if (!await SetApiKeyAsync(console))
@@ -32,11 +38,11 @@ namespace ImageOptimiser
             }
 
             var squasher = new ImageSquasher(console);
-            await squasher.SquashFileAsync(GetFilesToSquash(console, Path));
+            await squasher.SquashFileAsync(GetFilesToSquash(console, Path, TargetPath));
 
             console.WriteLine($"Compression complete.");
             console.WriteLine($"{Tinify.CompressionCount} compressions this month");
-
+            
             return Program.OK;
         }
 
@@ -67,19 +73,32 @@ namespace ImageOptimiser
             }
         }
 
-        static string[] GetFilesToSquash(IConsole console, string path)
+        static KeyValuePair<string,string>[] GetFilesToSquash(IConsole console, string path, string targetPath)
         {
             console.WriteLine($"Checking '{path}'...");
             if (File.GetAttributes(path).HasFlag(FileAttributes.Directory))
             {
-                console.WriteLine($"Path '{path}' is a directory, squashing all files");
-                return Directory.GetFiles(path);
+                console.WriteLine($"Path '{path}' is a directory, squashing all files in all directories, one level");
+                var filePaths = new List<KeyValuePair<string, string>>();
+                
+                foreach (var dirPath in Directory.GetDirectories(path))
+                {
+                    foreach(var filePath in Directory.GetFiles(dirPath)) {
+                        var parentDirectory = System.IO.Path.GetFileName(System.IO.Path.GetDirectoryName(filePath));
+                        var fileName = System.IO.Path.GetFileName(filePath);
+                        var targetFilePath = System.IO.Path.Combine(targetPath, parentDirectory, fileName);
+                        filePaths.Add(new KeyValuePair<string, string>(filePath, targetFilePath));
+                    }
+                }
+                return filePaths.ToArray();
             }
             else
             {
                 console.WriteLine($"Path '{path}' is a file");
-                return new[] { path };
+                return null;
             }
         }
+
+        
     }
 }
--- src/dotnet-tinify/ImageSquasher.cs
+++ src/dotnet-tinify/ImageSquasher.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using TinifyAPI;
+using System.Drawing;
 
 namespace ImageOptimiser
 {
@@ -15,29 +16,49 @@ namespace ImageOptimiser
 
         public ImageSquasher(IConsole console) => Console = console;
 
-        public async Task SquashFileAsync(IEnumerable<string> filesToSquash)
+        public async Task SquashFileAsync(IEnumerable<KeyValuePair<string, string>> filesToSquash)
         {
             if (filesToSquash is null) throw new ArgumentNullException(nameof(filesToSquash));            
 
             var compressFileTasks = filesToSquash
-                .Where(file => Constants.SupportedExtensions.Contains(Path.GetExtension(file)))
-                .Select(file => CompressFileAsync(file));
+                .Where(file => Constants.SupportedExtensions.Contains(Path.GetExtension(file.Key)))
+                .Select(file => CompressFileAsync(file.Key,file.Value));
 
             await Task.WhenAll(compressFileTasks);
         }
         
-        async Task CompressFileAsync(string file)
+        async Task CompressFileAsync(string file, string target)
         {
             try
             {
-                Console.WriteLine($"Compressing {Path.GetFileName(file)}...");
+                //check if target file already exists as a non-empty file
+                var targetInfo = new FileInfo(target);
+                if (targetInfo.Length > 0) return;
+                
+                
 
                 var originalSizeInBytes = new FileInfo(file).Length;
+                //return for empty file
+                if (originalSizeInBytes == 0) return;
 
+                var image = Image.FromFile(file);
+                
                 var source = Tinify.FromFile(file);
-                await source.ToFile(file);
+                var resized = source.Resize(new
+                {
+                    method = "scale",
+                    width = Math.Min(image.Width, 1920)
+                });
 
-                var newSizeInBytes = new FileInfo(file).Length;
+                if (!File.Exists(target)) {
+                    Directory.CreateDirectory(Path.GetDirectoryName(target));
+                    File.Create(target);
+                }
+
+                Console.WriteLine($"Compressing {Path.GetFileName(file)}...");
+                await resized.ToFile(target);
+
+                var newSizeInBytes = new FileInfo(target).Length;
                 var percentChange = (newSizeInBytes - originalSizeInBytes) * 100.0 / originalSizeInBytes;
 
                 Console.WriteLine($"Compression complete. {Path.GetFileName(file)} was {originalSizeInBytes.Bytes()}, now {newSizeInBytes.Bytes()} (-{percentChange:0}%)");
@@ -46,6 +67,16 @@ namespace ImageOptimiser
             {
                 Console.WriteLine($"An error occurred compressing {Path.GetFileName(file)}: ");
                 Console.WriteLine(ex);
+                //clean up already created file when an error occurs
+                if (!File.Exists(target))
+                {
+                    File.Delete(target);
+                    //check if folder is empty and delete
+                    if(!Directory.EnumerateFileSystemEntries(target).Any())
+                    {
+                        Directory.Delete(Path.GetDirectoryName(target));
+                    }
+                }
             }
         }
     }
